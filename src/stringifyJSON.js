6eeb47e48e545a5aa104677e4ef45b33f9c9097c
Katelyn Stamas <cakelyn@gmail.com>
2017-09-26 13:36:12 -0400

    Passed tests for stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,56 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
-/*
-
-  - JSON is used to exchange data to/from a web server.
-  - When sending data to a web server, the data has to be a string.
-  - Imagine you have this JavaScript object:
-
-      var obj = {
-        "name": "John",
-        "age": 30,
-        "city": "New York"
-      };
-
-  - You can use the JavaScript function JSON.stringify() to convert this object to a string
-
-    var myJSON = JSON.stringify(obj);
-
-  - The result in myJSON will be a string following the JSON notation
-  - myJSON is now a string and is ready to be sent to a server
-
-  SPECS
-  - Boolean, Number, and String objects are converted to the corresponding primitive
-    values during stringification
-  - If undefined, a function, or a symbol is encountered during conversion is either
-    ommitted or censored to null
-
-*/
 var stringifyJSON = function(obj) {
 
-  if (typeof obj === undefined) {
-    return;
+  if (obj === null) {
+    return "null";
+  }
+  if (typeof obj === 'undefined') {
+    return null;
   }
   if (typeof obj === 'function') {
-    return;
+    return null;
   }
   if (typeof obj === 'string') {
     return '"' + obj + '"';
   }
-  if (typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === null) {
-    // return as a string
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
   }
   if (Array.isArray(obj)) {
-    // pretty much the same as object for each element of the array
+    var arrayArr = [];
+    for (var i = 0; i < obj.length; i++) {
+      var arrVal = stringifyJSON(obj[i]);
+      arrayArr.push(arrVal);
+    }
+    return "[" + arrayArr.join() + "]";
   }
   if (typeof obj === 'object') {
+    var objectArr = [];
     for (var key in obj) {
-      stringifyJSON(obj[key]);
+      var objVal = stringifyJSON(obj[key]);
+      if (objVal !== null) {
+        objectArr.push('"' + key + '":' + objVal);
+      }
     }
-    // surround with curly braces for each field
-    // print the field name and all
-    // call the function recursively on the objects value, placing the returned string there
+    return "{" + objectArr.join() +  "}";
+
   }
 
 };
